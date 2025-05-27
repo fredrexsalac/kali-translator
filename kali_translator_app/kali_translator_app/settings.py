f30edf04a54db9e9599461a5a994acfc38214a0d
fredrexsalac <fredrexsalac@gmail.com>
2025-05-27 19:41:39 +0800

    Update settings.py

--- kali_translator_app/kali_translator_app/settings.py
+++ kali_translator_app/kali_translator_app/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kali_translator_app.urls'
@@ -79,4 +80,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
